5acec8c78a8db257e97362626fcac8fe050f6c15
Bartek <bart.matusz@gmail.com>
2020-03-26 00:37:32 +0100

    view registration using bike service, html done

--- bike-ui/src/app/components/view-registration/view-registration.component.ts
+++ bike-ui/src/app/components/view-registration/view-registration.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { BikeService } from 'src/app/services/bike.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-view-registration',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewRegistrationComponent implements OnInit {
 
-  constructor() { }
+  public bikeReg;
+
+  constructor(private bikeService: BikeService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.getBikeReg(this.route.snapshot.params.id);
+  }
+
+  getBikeReg(id: number){
+    this.bikeService.getBike(id).subscribe(
+      data => {
+        this.bikeReg = data;
+      },
+      err => console.log(err),
+      () => console.log('bike loaded')
+    );
   }
 
 }
