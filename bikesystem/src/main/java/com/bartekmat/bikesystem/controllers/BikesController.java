338e3c6c2cd005cdba42fefe7cb42b9c487d161c
Bartek <bart.matusz@gmail.com>
2020-04-03 22:04:08 +0200

    small fix refactor

--- bikesystem/src/main/java/com/bartekmat/bikesystem/controllers/BikesController.java
+++ bikesystem/src/main/java/com/bartekmat/bikesystem/controllers/BikesController.java
@@ -5,7 +5,6 @@ import com.bartekmat.bikesystem.repositories.BikeRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
